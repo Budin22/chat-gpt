e9c00f5f526e238d1bd162ef5afed075ab7c17b4
Sergii <apserbud22@gmail.com>
2025-04-04 17:25:01 +0300

    add chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import ChatWS from "@/components/ChatWS";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
+      <Suspense fallback={null}>
         <ChatWS />
+      </Suspense>
   );
 }
